b225c434dfb28145ae35d53a4030879b889da45b
Ramona <125778326+ramona013@users.noreply.github.com>
2023-06-20 03:47:14 +0200

    Initialize chromium-export-cl-2185190

---
+++ pointerevents/idlharness.window.js
@@ -0,0 +1,20 @@
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://w3c.github.io/pointerevents/
+
+idl_test(
+  ['pointerevents'],
+  ['uievents', 'html', 'dom'],
+  idl_array => {
+    idl_array.add_objects({
+      Document: ['document'],
+      Element: ['document'],
+      Window: ['window'],
+      Navigator: ['navigator'],
+      PointerEvent: ['new PointerEvent("type")']
+    });
+  }
+);
